b3a375324755139c82475c5fae46abf20097190c
zoeyourlife <38286505+zoeyourlife@users.noreply.github.com>
2023-04-04 21:37:23 +0900

    [#3] useWindowSize Custom hook (#4)
    브라우저창의 크기를 동적으로 제어하기 !

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,50 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import styled, { ThemeProvider } from "styled-components";
+import { PropsWithChildren, useEffect } from "react";
+import GlobalStyle from "../../styles/GlobalStyle";
+import theme from "../../styles/Theme/theme";
+import useWindowSize from "../hooks/useWindowSize";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Devway</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0 viewport-fit=cover"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Layout>
+          <Component {...pageProps} />;
+        </Layout>
+      </ThemeProvider>
+    </>
+  );
 }
+
+export default App;
+
+let vh = 0;
+
+function Layout({ children }: PropsWithChildren<{}>) {
+  const windowSize = useWindowSize();
+
+  useEffect(() => {
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }, [windowSize.height]);
+
+  return <StyledLayout>{children}</StyledLayout>;
+}
+
+const StyledLayout = styled.div`
+  min-height: calc(var(--var, 1vh) * 100);
+  max-width: 1440px;
+  width: 100vw;
+  margin: 0 auto;
+  background-color: ${({ theme }) => theme.color.white};
+`;
