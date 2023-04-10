ecc110998d99c4d92e5c959417e511d6a9d6d6c8
zoeyourlife <38286505+zoeyourlife@users.noreply.github.com>
2023-04-10 16:39:22 +0900

    Feat/#10 (#13)
    * [#10] Login Component Develop
    
    공용 버튼 및 input 창 개발 후 개발 예정.
    
    * [#10] Login Component - firebase apply
    
    Firebase 연동 및 설정 ing

--- src/Components/Common/LoginLottie/index.tsx
+++ src/Components/Common/LoginLottie/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Lottie, { Options } from "react-lottie";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 interface ILoginlottieProps {
   lottieData: object;
@@ -25,13 +25,69 @@ function LoginLottie({ width, height, lottieData }: ILoginlottieProps) {
         height={height}
         isClickToPauseDisabled
       />
-      ;
+      <StyledTextSpan>
+        <StyledH1>Mate. Make. Together.</StyledH1>
+        <StyledP>로그인 또는 계정 만들기</StyledP>
+      </StyledTextSpan>
     </StyledWrapper>
   );
 }
 
 export default LoginLottie;
 
+const media = {
+  desktop: (styles: string) => css`
+    @media only screen and (max-width: 1024px) {
+      ${styles}
+    }
+  `,
+  tablet: (styles: string) => css`
+    @media only screen and (min-width: 576px) and (max-width: 767px) {
+      ${styles}
+    }
+  `,
+  mobile: (styles: string) => css`
+    @media only screen and (max-width: 575px) {
+      ${styles}
+    }
+  `,
+};
+
 const StyledWrapper = styled.div`
+  position: relative;
   width: 100%;
+  margin: 0 auto;
+  margin-top: 3.125rem;
+  ${media.desktop(`
+    min-width: 1024px;
+    display: none;
+  `)}
+
+  ${media.tablet(`
+    min-width: 768px;
+    display: none;
+  `)}
+
+  ${media.mobile(`
+    min-width: 576px;
+    display: none;
+  `)}
+`;
+
+const StyledTextSpan = styled.span`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 40%;
+  left: 26%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledH1 = styled.h1`
+  color: ${({ theme }) => theme.color.black};
+`;
+
+const StyledP = styled.p`
+  color: ${({ theme }) => theme.color.black};
 `;
---
+++ src/constants/firebaseConfig.ts
@@ -0,0 +1,44 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import {
+  API_KEY,
+  APP_ID,
+  AUTH_DOMAIN,
+  MEASUREMENT_ID,
+  MESSAGING_SENDER_ID,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+} from "./firebaseUrl";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: API_KEY as string,
+  authDomain: AUTH_DOMAIN as string,
+  projectId: PROJECT_ID as string,
+  storageBucket: STORAGE_BUCKET as string,
+  messagingSenderId: MESSAGING_SENDER_ID as string,
+  appId: APP_ID as string,
+  measurementId: MEASUREMENT_ID as string,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);
+const provider = new GoogleAuthProvider();
+
+// 사용자가 선호하는 언어로 현지화
+auth.languageCode = "kr";
+
+// 0Auth 요청과 함께 전송할 커스텀 0Auth 제공업체의 매개변수 추가 지정
+provider.setCustomParameters({
+  login_hint: "user@example.com",
+});
+
+export { app, auth };
