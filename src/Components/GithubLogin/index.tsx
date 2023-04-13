4a11d8d26c9027281ad20b7ccb8ee2a7177e6892
zoeyourlife <201814112@office.skhu.ac.kr>
2023-04-13 15:20:17 +0900

    [#14] Github Login error solve
    깃헙로그인 안되던 문제 해결, 파베 콘솔에 로그인 완료

--- src/Components/GithubLogin/index.tsx
+++ src/Components/GithubLogin/index.tsx
@@ -1,40 +1,80 @@
-import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
+import { errorMonitor } from "events";
+import {
+  GithubAuthProvider,
+  User,
+  signInWithPopup,
+  signInWithRedirect,
+} from "firebase/auth";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
 import { useRouter } from "next/router";
-import { auth } from "src/constants/firebaseConfig";
+import { auth, db } from "src/constants/firebaseConfig";
 import styled from "styled-components";
 
 function GithubLogin() {
   const router = useRouter();
   const githubProvider = new GithubAuthProvider();
 
-  const signInwithPopup = async () => {
-    signInWithPopup(auth, githubProvider)
-      .then((res) => {
-        githubProvider.setCustomParameters({
-            prompt: "select_account",
-        });
-        const credential = GithubAuthProvider.credentialFromResult(res);
-        const token = credential?.accessToken;
-
-        // when login success, router main page
-        router.push("/");
-
-        // The Signed-in user Info.
-        const user = res.user;
-        // IdP data avaliable using getAdditionalUserInfo(res)
-      })
-      .catch((err) => {
-        const errCode = err.code;
-        const errMessage = err.message;
-        const email = err.customData.email;
-        const credential = GithubAuthProvider.credentialFromError(err);
+  const signInWithGithub = async () => {
+    try {
+      githubProvider.setCustomParameters({
+        prompt: "select_account",
       });
+      const res = await signInWithPopup(auth, githubProvider);
+      const user = res.user;
+      console.log(user);
+      router.push("/");
+
+      await createUserIfNotExists(user);
+    } catch (error) {
+      if (error.code === "auth/account-exists-with-different-credential") {
+        console.log("이미 이메일이 존재합니다?");
+      } else {
+        console.log(error);
+      }
+    }
   };
+
+  const createUserIfNotExists = async (user: User) => {
+    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName || user.email,
+        authProvider: "github",
+        email: user.email,
+      });
+    }
+  };
+
+  //   const signInwithPopup = async () => {
+  //     githubProvider.setCustomParameters({
+  //         prompt: "select_account",
+  //     });
+  //     signInWithPopup(auth, githubProvider)
+  //       .then((res) => {
+  //         const credential = GithubAuthProvider.credentialFromResult(res);
+  //         const token = credential?.accessToken;
+
+  //         // when login success, router main page
+  //         router.push("/");
+
+  //         // The Signed-in user Info.
+  //         const user = res.user;
+  //         // IdP data avaliable using getAdditionalUserInfo(res)
+  //       })
+  //       .catch((err) => {
+  //         const errCode = err.code;
+  //         const errMessage = err.message;
+  //         const email = err.customData.email;
+  //         const credential = GithubAuthProvider.credentialFromError(err);
+  //       });
+  //   }
 
   return (
     <StyledDiv>
       <h3>깃헙 로그인 테스트</h3>
-      <button onClick={signInwithPopup}>로그인</button>
+      <button onClick={signInWithGithub}>로그인</button>
     </StyledDiv>
   );
 }
--- src/Components/GoogleLogin/index.tsx
+++ src/Components/GoogleLogin/index.tsx
@@ -17,6 +17,7 @@ function GoogleLogin() {
       });
       const res = await signInWithPopup(auth, googleProvider);
       const user = res.user;
+      console.log(user);
       const q = query(collection(db, "users"), where("uid", "==", user.uid));
       const docs = await getDocs(q);
       if (docs.docs.length === 0) {
