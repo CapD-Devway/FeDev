7fd734edbc328424f04d37d4d2cd51c5db485466
zoeyourlife <201814112@office.skhu.ac.kr>
2023-05-31 06:55:39 +0900

    [#30] TeamProfile Component Setup
    feat: 팀 프로필 컴포넌트 셋업

---
+++ src/pages/TeamProfile/index.tsx
@@ -0,0 +1,13 @@
+import Nav from "src/Components/Nav";
+import BottomNav from "src/Components/Nav/BottomNav";
+
+function TeamProfile() {
+  return (
+    <>
+      <Nav />
+      <BottomNav />
+    </>
+  );
+}
+
+export default TeamProfile;
